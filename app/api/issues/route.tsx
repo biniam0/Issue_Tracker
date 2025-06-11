a547de1132cc7c6bdeda82c661fc87b418027bac
bini <jaletakebede@gmail.com>
2025-06-11 13:10:50 +0300

    Build an api for creating issues

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import issueSchema from "./issueSchema";
+import { prisma } from "@/prisma/client";
+
+const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+};
+
+export { POST };
