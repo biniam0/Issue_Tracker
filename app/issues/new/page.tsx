eaa1f22b79b0f6ddee56c693ef93d4ef24690a6f
bini <jaletakebede@gmail.com>
2025-06-11 15:14:23 +0300

    Handle Issue submission with react hook form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,48 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
 import { Button, TextField } from "@radix-ui/themes";
-import MarkDownEditor from "./MarkDownEditor";
+import dynamic from "next/dynamic";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface IssueType {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, setValue, watch } = useForm<IssueType>();
+  const description = watch("description", "");
+
+  const handleCreateIssue = (data: IssueType) => {
+    axios.post("/api/issues", data);
+    router.push("/issues");
+  };
+
   return (
-    <div className="max-w-lg space-y-3">
-      <TextField.Root placeholder="Title: Bug Fix"></TextField.Root>
-      <MarkDownEditor />
+    <form
+      className="max-w-lg space-y-3"
+      onSubmit={handleSubmit((data) => handleCreateIssue(data))}
+    >
+      <TextField.Root
+        placeholder="Title: Bug Fix"
+        {...register("title")}
+      ></TextField.Root>
+      <SimpleMDE
+        placeholder="Description"
+        value={description}
+        onChange={(value) => setValue("description", value)}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
