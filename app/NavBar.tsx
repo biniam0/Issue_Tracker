83dbd5b8608db39ca1a04d72e4e5a7f140f317ba
bini <jaletakebede@gmail.com>
2025-06-11 11:49:12 +0300

    Refactor layout and styling; add NavBar component and update dependencies Bug Fix: tailwindcss not being applied

---
+++ app/NavBar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug size="26px" />
+      </Link>
+      <ul className="flex gap-6 text-sm">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="p-1 m-4 rounded-sm w-fit bg-slate-500 text-white font-bold text-xl">
+      Hello World
+    </div>
+  );
 }
