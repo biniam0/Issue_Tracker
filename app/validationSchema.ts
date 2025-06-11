c04c4e9f4c01f8daa415131abbaea967d1c7a022
bini <jaletakebede@gmail.com>
2025-06-11 17:00:50 +0300

    Refactor issue validation schema and integrate with form handling Implement client side validation

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-const issueSchema = z.object({
+const createIssueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
   description: z.string().min(1, "Description is required"),
 });
 
-export default issueSchema;
+export default createIssueSchema;
