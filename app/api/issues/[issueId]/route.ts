018a781b3eee00974a7ddc36675969963d03d4b5
bini <jaletakebede@gmail.com>
2025-06-12 13:54:00 +0300

    Build delete API and delete functionality

--- app/api/issues/[issueId]/route.ts
+++ app/api/issues/[issueId]/route.ts
@@ -35,4 +35,28 @@ const PATCH = async (
   return NextResponse.json(updatedIssue, { status: 200 });
 };
 
-export { PATCH };
+const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { issueId: string } }
+) => {
+  const { issueId } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(issueId),
+    },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  const deletedIssue = await prisma.issue.delete({
+    where: {
+      id: parseInt(issueId),
+    },
+  });
+  return NextResponse.json(deletedIssue);
+};
+
+
+export { PATCH, DELETE };
