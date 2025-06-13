3d4d1fb6868cf768f2a34ddeb64f80a27e7d257e
bini <jaletakebede@gmail.com>
2025-06-13 12:06:17 +0300

    added loading skeleton for while fetching user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,9 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const GET = async (req: NextRequest) => {
   const users = await prisma.user.findMany({orderBy: {name: "asc" }});
-  console.log("Users: ====> ", users);
-  if (!users) return NextResponse.json("Users not found", { status: 404 });
-
+  
   return NextResponse.json(users, { status: 200 });
 };
 
--- app/issues/[issueId]/AssigneeSelect.tsx
+++ app/issues/[issueId]/AssigneeSelect.tsx
@@ -1,27 +1,56 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { User } from "@prisma/client";
 import { Box, Select } from "@radix-ui/themes";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const AssigneeSelect = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        setIsLoading(true);
         const users = await axios
           .get<User[]>("/api/users")
           .then((res) => res.data);
         setUsers(users);
       } catch (error) {
-        console.log(error);
+        console.log("Error while fetching for user", error);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchUser();
   }, []);
 
+  // const fetchUser = async () => {
+  //   try {
+  //     return axios.get("/api/users").then((res) => res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //     return error;
+  //   }
+  // };
+
+  // const {
+  //   data: users,
+  //   error,
+  //   isLoading,
+  // } = useQuery<User[]>({
+  //   queryKey: ["users"],
+  //   queryFn: fetchUser,
+  //   staleTime: 60 * 1000,
+  //   retry: 3,
+  // });
+
+  if (isLoading) return <Skeleton height="2rem" />;
+
+  // if (error) return null;
   return (
     <Box width="100px" asChild>
       <Select.Root size="2">
@@ -29,7 +58,7 @@ const AssigneeSelect = () => {
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            {users.map((user) => (
+            {users?.map((user) => (
               <Select.Item key={user.id} value={user.id}>
                 {user.name}
               </Select.Item>
