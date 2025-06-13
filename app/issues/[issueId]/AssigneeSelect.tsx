9afe66e2198ecdbd3ff6be110c058c7dff4677de
bini <jaletakebede@gmail.com>
2025-06-13 14:36:42 +0300

    Wrap the app with React query custom component and used react query

--- app/issues/[issueId]/AssigneeSelect.tsx
+++ app/issues/[issueId]/AssigneeSelect.tsx
@@ -3,14 +3,12 @@
 import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-
   const handleSelectUser = async (userId: string) => {
     await axios
       .patch(`/api/issues/${issue.id}`, {
@@ -19,38 +17,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       .catch(() => toast.error("Changes could not be saved."));
   };
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        setIsLoading(true);
-        const users = await axios
-          .get<User[]>("/api/users")
-          .then((res) => res.data);
-        setUsers(users);
-      } catch (error) {
-        console.log("Error while fetching for user", error);
-        setIsLoading(false);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchUser();
-  }, []);
-
-  // const fetchUser = async () => {
-  //   try {
-  //     return axios.get("/api/users").then((res) => res.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //     return error;
-  //   }
-  // };
-
-  // const {
-  //   data: users,
-  //   error,
-  //   isLoading,
-  // } = useUsers()
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton height="2rem" />;
 
@@ -80,12 +47,21 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   );
 };
 
-// const useUsers = () =>
-//   useQuery<User[]>({
-//     queryKey: ["users"],
-//     queryFn: fetchUser,
-//     staleTime: 60 * 1000,
-//     retry: 3,
-//   });
+const fetchUser = async () => {
+  try {
+    return axios.get("/api/users").then((res) => res.data);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: fetchUser,
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
 export default AssigneeSelect;
