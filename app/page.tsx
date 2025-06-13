650cb45d78cfcf856fe74a5da86288c289f35e1f
bini <jaletakebede@gmail.com>
2025-06-14 01:41:43 +0300

    Build the issue chart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+import { prisma } from "@/prisma/client";
+import IssuesChart from "./IssuesChart";
 import IssuesSummary from "./IssuesSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
+const counts = {
+  OPEN: 0,
+  IN_PROGRESS: 0,
+  CLOSED: 0,
+};
+
+const Home = async () => {
+  const groupedCounts = await prisma.issue.groupBy({
+    by: ["status"],
+    _count: {
+      status: true,
+    },
+  });
+
+  groupedCounts.forEach(({ status, _count }) => {
+    counts[status] = _count.status;
+  });
+
+  const { OPEN: open, CLOSED: closed, IN_PROGRESS: inProgress } = counts;
   return (
     <>
       <LatestIssues />
-      <IssuesSummary />
+      <IssuesSummary open={open} closed={closed} inProgress={inProgress} />
+      <IssuesChart open={open} closed={closed} inProgress={inProgress} />
     </>
   );
-}
+};
+
+export default Home;
