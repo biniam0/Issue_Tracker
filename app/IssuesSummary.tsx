8ccdc64969286762d189e230e516932410a2e812
bini <jaletakebede@gmail.com>
2025-06-14 01:41:43 +0300

    Build the issue chart component

--- app/IssuesSummary.tsx
+++ app/IssuesSummary.tsx
@@ -3,25 +3,13 @@ import { Status } from "@prisma/client";
 import { Card, Flex, Text } from "@radix-ui/themes";
 import Link from "next/link";
 
-const counts = {
-  OPEN: 0,
-  IN_PROGRESS: 0,
-  CLOSED: 0,
-};
+interface Props {
+  open: number;
+  closed: number;
+  inProgress: number;
+}
 
-const IssuesSummary = async () => {
-  const groupedCounts = await prisma.issue.groupBy({
-    by: ["status"],
-    _count: {
-      status: true,
-    },
-  });
-
-  groupedCounts.forEach(({ status, _count }) => {
-    counts[status] = _count.status;
-  });
-
-  const { OPEN: open, CLOSED: closed, IN_PROGRESS: inProgress } = counts;
+const IssuesSummary = async ({ open, closed, inProgress }: Props) => {
 
   const statuses: { label: string; value: number; status: Status }[] = [
     { label: "Open Issues", value: open, status: "OPEN" },
