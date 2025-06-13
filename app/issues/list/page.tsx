2a50e957e0328ac4412b15f17f3c537cc8ef5ce8
bini <jaletakebede@gmail.com>
2025-06-13 19:10:32 +0300

    Make column sort the issue

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,20 +1,32 @@
 import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
+import NextLink from "next/link";
 import IssueActionPage from "./IssueAction";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 interface Props {
   searchParams: {
-    status: Status | "all"
+    status: Status | "all";
+    orderBy: keyof Issue;
   };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
+  const statuses = [...Object.values(Status), "all"];
+  const columns: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
   let { status } = await searchParams;
 
-  const statuses = [...Object.values(Status), "all"]
-  status = statuses.includes(status) ? status : "all"
+  status = statuses.includes(status) ? status : "all";
 
   let issues = await prisma.issue.findMany();
 
@@ -32,16 +44,21 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Description
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="block md:hidden">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <NextLink
+                  href={{ query: { ...searchParams, orderBy: column.value } }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <AiOutlineArrowUp className="pl-1 inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
@@ -50,15 +67,12 @@ const IssuesPage = async ({ searchParams }: Props) => {
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="hidden md:block">
+                <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.RowHeaderCell>
-              <Table.Cell className="block md:hidden">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.description}
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
