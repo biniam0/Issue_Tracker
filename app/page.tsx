75d520ff7b12818d11409990683743ef266f01c0
bini <jaletakebede@gmail.com>
2025-06-13 19:10:32 +0300

    Make column sort the issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="p-1 m-4 rounded-sm w-fit bg-slate-500 text-white font-bold text-xl">
+    <div className="p-1 m-4 rounded-sm w-fit bg-blue-500 text-white font-medium text-xl">
       Hello World
     </div>
   );
